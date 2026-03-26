284ae51113755d072e23f30b3b2e85fa876a5df8
Versus1478 <svider.vladyslav@gmail.com>
2026-03-26 16:35:03 +0100

    Implement TaskController destroy, store, index and show methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,25 +12,75 @@ class TaskController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(string $noteId)
     {
-        //
+        $note = Note::find($noteId);
+
+        if (!$note) {
+            return response()->json([
+                'message' => 'Poznámka nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([
+            'tasks' => $note->tasks()->get()
+        ], Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, string $noteId)
     {
-        //
+        $note = Note::find($noteId);
+
+        if (!$note) {
+            return response()->json([
+                'message' => 'Poznámka nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $validated = $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'is_done' => ['sometimes', 'boolean'],
+            'due_at' => ['nullable', 'date']
+        ]);
+
+        $task = $note->tasks()->create($validated);
+
+        return response()->json([
+            'message' => 'Úloha bola vytvorená.',
+            'task' => $task
+        ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $noteId, string $taskId)
     {
-        //
+        $note = Note::find($noteId);
+
+        if (!$note) {
+            return response()->json([
+                'message' => 'Poznámka nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $task = $note->tasks()
+            ->with('comments.user:id,first_name,last_name')
+            ->where('id', $taskId)
+            ->first();
+
+        if (!$task) {
+            return response()->json([
+                'message' => 'Úloha nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([
+            'task' => $task
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -71,8 +121,28 @@ class TaskController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $noteId, string $taskId)
     {
-        //
+        $note = Note::find($noteId);
+
+        if (!$note) {
+            return response()->json([
+                'message' => 'Poznámka nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $task = $note->tasks()->where('id', $taskId)->first();
+
+        if (!$task) {
+            return response()->json([
+                'message' => 'Úloha nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Úloha bola odstránená.'
+        ], Response::HTTP_OK);
     }
 }
