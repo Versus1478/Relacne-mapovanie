1cc226a7fd50545a31803eeb0e95c88a1d069e26
Versus1478 <svider.vladyslav@gmail.com>
2026-03-26 15:53:48 +0100

    Load full relations in NoteController show method

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -77,13 +77,22 @@ class NoteController extends Controller
 
     public function show(string $id)
     {
-        $note = Note::find($id);
+        $note = Note::with([
+            'user:id,first_name,last_name',
+            'categories:id,name,color',
+            'comments.user:id,first_name,last_name',
+            'tasks.comments.user:id,first_name,last_name'
+        ])->find($id);
 
         if (!$note) {
-            return response()->json(['message' => 'Poznámka nenájdená.'], Response::HTTP_NOT_FOUND);
+            return response()->json([
+                'message' => 'Poznámka nenájdená.'
+            ], Response::HTTP_NOT_FOUND);
         }
 
-        return response()->json(['note' => $note], Response::HTTP_OK);
+        return response()->json([
+            'note' => $note
+        ], Response::HTTP_OK);
     }
 
     /**
